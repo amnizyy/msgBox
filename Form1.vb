a4b2c5839d052c01a0d1afdbf9c7d4c11f2f7994
amnizyy <46371731+amnizyy@users.noreply.github.com>
2020-09-26 17:09:33 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        lbldisplay.Text = InputBox("Enter your name", "Input Box")
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim intresult As Integer
+
+        intresult = messagebox.Show("Are you sure", "EXIT", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+        If intresult = Windows.Forms.DialogResult.Yes Then
+            End
+        ElseIf intresult = Windows.Forms.DialogResult.No Then
+
+            Me.Show()
+
+        End If
+
+    End Sub
+End Class
